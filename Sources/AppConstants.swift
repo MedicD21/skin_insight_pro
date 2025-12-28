f8dabae22ed8a947be6c7be35eaa3699ad123eb3
MedicD21 <DSchaaf12@me.com>
2025-12-28 08:34:45 -0500

    updates

--- Sources/AppConstants.swift
+++ Sources/AppConstants.swift
@@ -2,3 +2,7 @@ struct AppConstants {
   static let appId = "1c2c6725-f7ca-4fd5-860a-bef747d136a2"
   static let baseUrl = "https://api.lastapp.ai"
 }
+struct AppConstants {
+    static let supabaseUrl = "https://meqrnevrimzvvhmopxrq.supabase.co" // Your Supabase Project URL
+    static let supabaseAnonKey = "sb_publishable_JYadlrb2j6E_jMlTL-fLsg_SjhuJqrg" // Your Supabase anon (public) key
+}
